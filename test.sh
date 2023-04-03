6587d1fc6e28cc56e278201ecd09c7beada965b1
Maggie Zhang <zzy@MaggiedeMacBook-Pro.local>
2023-04-03 13:43:40 +0800

    remove answer

--- test.sh
+++ test.sh
@@ -4,6 +4,6 @@ NAME=$(sudo docker run -d -v ${SHARE}:/host/Users -it ${IMAGE} /bin/bash)
 echo '****************'
 sudo docker exec -i $NAME ./ask /host/Users/data/set1/a1.txt 3 2>/dev/null
 echo '****************'
-sudo docker exec -i $NAME ./answer /host/Users/data/set1/a1.txt /host/Users/test_questions.txt
-echo '****************'
-sudo docker stop $NAME >/dev/null
+#sudo docker exec -i $NAME ./answer /host/Users/data/set1/a1.txt /host/Users/test_questions.txt
+#echo '****************'
+#sudo docker stop $NAME >/dev/null
