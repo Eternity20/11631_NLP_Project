4efa929377864a3205aa5448926c3db6d009f066
Maggie <zhongyuezhang785@gmail.com>
2023-04-04 00:11:01 +0800

    merge 3 model together

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@ SHARE=$(pwd)
 IMAGE=$1
 NAME=$(sudo docker run -d -v ${SHARE}:/host/Users -it ${IMAGE} /bin/bash)
 echo '****************'
-sudo docker exec -i $NAME ./ask /host/Users/data/set1/a2.txt 5 2>/dev/null
+sudo docker exec -i $NAME ./ask /host/Users/data/set1/a2.txt 11 2>/dev/null
 echo '****************'
 #sudo docker exec -i $NAME ./answer /host/Users/data/set1/a1.txt /host/Users/test_questions.txt
 #echo '****************'
