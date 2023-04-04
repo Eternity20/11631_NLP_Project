fd470311b03b0b6ab5d2175f9f6c1b740d8a5298
Maggie <zhongyuezhang785@gmail.com>
2023-04-04 10:40:07 +0800

    remove the verbose for QA

--- test.sh
+++ test.sh
@@ -4,6 +4,6 @@ NAME=$(sudo docker run -d -v ${SHARE}:/host/Users -it ${IMAGE} /bin/bash)
 #echo '*******QG*********'
 #sudo docker exec -i $NAME ./ask /host/Users/data/wikipedia_text/pokemon_characters/pikachu_pokemon.txt 3 2>/dev/null
 echo '*******QA*********'
-sudo docker exec -i $NAME ./answer /host/Users/data/wikipedia_text/pokemon_characters/pikachu_pokemon.txt /host/Users/test_questions.txt 2 > /dev/null
+sudo docker exec -i $NAME ./answer /host/Users/data/wikipedia_text/pokemon_characters/pikachu_pokemon.txt /host/Users/test_questions.txt 2>/dev/null
 echo '*****Finish********'
 sudo docker stop $NAME >/dev/null
