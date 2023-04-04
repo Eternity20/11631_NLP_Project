962bbd72760333a5c353d61c13ca6ca7757b7671
Maggie <zhongyuezhang785@gmail.com>
2023-04-04 10:09:24 +0800

    revise with the test.sh

--- test.sh
+++ test.sh
@@ -1,9 +1,9 @@
 SHARE=$(pwd)
 IMAGE=$1
 NAME=$(sudo docker run -d -v ${SHARE}:/host/Users -it ${IMAGE} /bin/bash)
-echo '****************'
+echo '*******QG*********'
 sudo docker exec -i $NAME ./ask /host/Users/data/wikipedia_text/pokemon_characters/pikachu_pokemon.txt 3 2>/dev/null
-echo '****************'
+echo '*******QA*********'
 sudo docker exec -i $NAME ./answer /host/Users/data/wikipedia_text/pokemon_characters/pikachu_pokemon.txt /host/Users/test_questions.txt
-echo '****************'
+echo '*****Finish********'
 sudo docker stop $NAME >/dev/null
