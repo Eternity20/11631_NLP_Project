0a1197cad4517742ffdd3496f7737bab98696ecd
Maggie <zhongyuezhang785@gmail.com>
2023-04-04 09:07:41 +0800

    update test data

--- test.sh
+++ test.sh
@@ -2,8 +2,8 @@ SHARE=$(pwd)
 IMAGE=$1
 NAME=$(sudo docker run -d -v ${SHARE}:/host/Users -it ${IMAGE} /bin/bash)
 echo '****************'
-sudo docker exec -i $NAME ./ask /host/Users/data/set1/a2.txt 11 2>/dev/null
+sudo docker exec -i $NAME ./ask /host/Users/data/wikipedia_text/pokemon_characters/pikachu_pokemon.txt 4 2>/dev/null
 echo '****************'
-#sudo docker exec -i $NAME ./answer /host/Users/data/set1/a1.txt /host/Users/test_questions.txt
+#sudo docker exec -i $NAME ./answer /host/Users/data/wikipedia_text/pokemon_characters/pikachu_pokemon.txt /host/Users/test_questions.txt
 #echo '****************'
 #sudo docker stop $NAME >/dev/null
