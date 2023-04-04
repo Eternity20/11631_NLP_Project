6505e6caf4fcf279da07d6085cd2abb6761c09a5
Maggie <zhongyuezhang785@gmail.com>
2023-04-04 12:37:59 +0800

    revise utils

--- docker/utils.py
+++ docker/utils.py
@@ -2,8 +2,10 @@ import torch
 
 def set_device():
 	if torch.cuda.is_available():
+		print("cuda")
 		device = torch.device('cuda')
 	else:
+		print("cpu")
 		device = torch.device('cpu')
 	#logger.info(f'Using Device: {device}')
 	return device
