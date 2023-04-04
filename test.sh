d939b2595b55437968f0b36c8f6be83344e7b7cf
Maggie <zhongyuezhang785@gmail.com>
2023-04-04 11:04:07 +0800

    display the information for QG

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 SHARE=$(pwd)
 IMAGE=$1
 NAME=$(sudo docker run -d -v ${SHARE}:/host/Users -it ${IMAGE} /bin/bash)
-#echo '*******QG*********'
+echo '*******QG*********'
 sudo docker exec -i $NAME ./ask /host/Users/data/wikipedia_text/pokemon_characters/pikachu_pokemon.txt 3 2>/dev/null
 echo '*******QA*********'
 sudo docker exec -i $NAME ./answer /host/Users/data/wikipedia_text/pokemon_characters/pikachu_pokemon.txt /host/Users/test_questions.txt 2>/dev/null
