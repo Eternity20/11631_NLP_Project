e6f6e6f1a17130f4a28f87d1163f75613b78f1cf
Maggie <zhongyuezhang785@gmail.com>
2023-04-14 01:09:33 -0700

    change with nvidia

--- test.sh
+++ test.sh
@@ -1,9 +1,9 @@
 SHARE=$(pwd)
 IMAGE=$1
-NAME=$(sudo docker run -d -v ${SHARE}:/host/Users -it ${IMAGE} /bin/bash --env CUDA_VISIBLE_DEVICES=0)
+NAME=$(sudo nvidia-docker run -d -v ${SHARE}:/host/Users -it ${IMAGE} /bin/bash --env CUDA_VISIBLE_DEVICES=0)
 #echo '*******QG*********'
 #sudo docker exec -i $NAME ./ask /host/Users/data/wikipedia_text/pokemon_characters/pikachu_pokemon.txt 3 2>/dev/null
 echo '*******QA*********'
-sudo docker exec -i $NAME ./answer /host/Users/data/wikipedia_text/pokemon_characters/pikachu_pokemon.txt /host/Users/test_questions.txt 2>/dev/null
+sudo nvidia-docker exec -i $NAME ./answer /host/Users/data/wikipedia_text/pokemon_characters/pikachu_pokemon.txt /host/Users/test_questions.txt 2>/dev/null
 echo '*****Finish********'
-sudo docker stop $NAME >/dev/null
+sudo nvidia-docker stop $NAME >/dev/null
