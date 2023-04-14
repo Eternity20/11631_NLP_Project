8fad1f66a3b4c74cf9dd088ccb5a2320185be4ac
Maggie <zhongyuezhang785@gmail.com>
2023-04-14 01:16:19 -0700

    install pytorch

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 SHARE=$(pwd)
 IMAGE=$1
-NAME=$(sudo nvidia-docker run -d -v ${SHARE}:/host/Users -it ${IMAGE} /bin/bash --env CUDA_VISIBLE_DEVICES=0)
+NAME=$(sudo nvidia-docker run -d -v ${SHARE}:/host/Users -it ${IMAGE} /bin/bash)
 #echo '*******QG*********'
 #sudo docker exec -i $NAME ./ask /host/Users/data/wikipedia_text/pokemon_characters/pikachu_pokemon.txt 3 2>/dev/null
 echo '*******QA*********'
