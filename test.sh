8f296a7937bfdef36363c029e07728590f29d9dc
Maggie <zhongyuezhang785@gmail.com>
2023-04-14 23:57:57 -0700

    change with gpu

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 SHARE=$(pwd)
 IMAGE=$1
-NAME=$(sudo docker run -d -v ${SHARE}:/host/Users -it ${IMAGE} /bin/bash --gpus all nvidia/cuda:12.0.0-base-ubuntu20.04)
+NAME=$(sudo docker run --runtime=nvidia -d -v ${SHARE}:/host/Users -it ${IMAGE} /bin/bash)
 #echo '*******QG*********'
 #sudo docker exec -i $NAME ./ask /host/Users/data/wikipedia_text/pokemon_characters/pikachu_pokemon.txt 3 2>/dev/null
 echo '*******QA*********'
