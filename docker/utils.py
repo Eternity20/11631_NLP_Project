aef69a5b7729db727ca0472a9572dbdfe9b69c52
Maggie <zhongyuezhang785@gmail.com>
2023-04-15 00:03:28 -0700

    GPU Work but without removing GPU

--- docker/utils.py
+++ docker/utils.py
@@ -4,10 +4,10 @@ def set_device():
 	print(f'Is CUDA available: {torch.cuda.is_available()}')
 	print(f'Nr of Devices:{torch.cuda.device_count()}')
 	if torch.cuda.is_available():
-		print("cuda")
+		# print("cuda")
 		device = torch.device('cuda')
 	else:
-		print("cpu")
+		# print("cpu")
 		device = torch.device('cpu')
 	#logger.info(f'Using Device: {device}')
 	return device
