5f35724fecb9d28e48870189b05e90ff7be070cd
Maggie <zhongyuezhang785@gmail.com>
2023-04-17 15:36:36 -0700

    change test.sh with a7

--- dryrun-testing/test.sh
+++ dryrun-testing/test.sh
@@ -2,7 +2,7 @@ SHARE=$(pwd)
 IMAGE=$1
 NAME=$(docker run -d -v ${SHARE}:/host/Users -it ${IMAGE} /bin/bash)
 echo '****************'
-docker exec -i $NAME ./ask /host/Users/a7.txt 3 2>/dev/null
+docker exec -i $NAME ./ask /host/Users/a7.txt 5 2>/dev/null
 #echo '****************'
 #docker exec -i $NAME ./answer /host/Users/a7.txt /host/Users/a7_questions.txt
 #echo '****************'
--- test.sh
+++ test.sh
@@ -2,7 +2,8 @@ SHARE=$(pwd)
 IMAGE=$1
 NAME=$(sudo docker run --runtime=nvidia -d -v ${SHARE}:/host/Users -it ${IMAGE} /bin/bash)
 echo '*******QG*********'
-sudo docker exec -i $NAME ./ask /host/Users/data/wikipedia_text/pokemon_characters/pikachu_pokemon.txt 3 2>/dev/null
+#sudo docker exec -i $NAME ./ask /host/Users/data/wikipedia_text/pokemon_characters/pikachu_pokemon.txt 3 2>/dev/null
+sudo docker exec -i $NAME ./ask /host/Users/a7.txt 3 2>/dev/null
 echo '*******QA*********'
 sudo docker exec -i $NAME ./answer /host/Users/data/wikipedia_text/pokemon_characters/pikachu_pokemon.txt /host/Users/test_questions.txt 2>/dev/null
 echo '*****Finish********'
