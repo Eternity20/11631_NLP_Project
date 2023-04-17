48cbc8a1b2c9b270e8218fd9311d1be6eb66aa99
Maggie <zhongyuezhang785@gmail.com>
2023-04-17 15:31:01 -0700

    try with dry-run

--- dryrun-testing/test.sh
+++ dryrun-testing/test.sh
@@ -1,9 +1,11 @@
 SHARE=$(pwd)
 IMAGE=$1
-NAME=$(docker run --runtime=nvidia -d -v ${SHARE}:/host/Users -it ${IMAGE} /bin/bash)
+NAME=$(docker run -d -v ${SHARE}:/host/Users -it ${IMAGE} /bin/bash)
 echo '****************'
-docker exec -i $NAME ./ask /host/Users/a7.txt 5 2>/dev/null
+docker exec -i $NAME ./ask /host/Users/a7.txt 3 2>/dev/null
 #echo '****************'
 #docker exec -i $NAME ./answer /host/Users/a7.txt /host/Users/a7_questions.txt
 #echo '****************'
 #docker stop $NAME >/dev/null
+
+docker run -v /home/ubuntu/11631_NLP_Project/dryrun-testing:/host/Users -it zhongyue/qaqg:gpu /bin/bash
