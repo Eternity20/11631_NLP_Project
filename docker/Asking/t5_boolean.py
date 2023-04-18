b58d26afc0a7b89d6d0e962a99e236cd1f1059b0
Gustavo <ggoncav@cs.cmu.edu>
2023-04-17 23:59:42 -0400

    Improvements to boolean T5 generation speed

--- docker/Asking/t5_boolean.py
+++ docker/Asking/t5_boolean.py
@@ -1,48 +1,49 @@
 import transformers
-transformers.logging.set_verbosity_error()
-transformers.logging.disable_progress_bar()
+#transformers.logging.set_verbosity_error()
+#transformers.logging.disable_progress_bar()
 import sentencepiece
-from transformers import T5ForConditionalGeneration,T5Tokenizer
+from transformers import T5ForConditionalGeneration,T5Tokenizer,T5TokenizerFast
 import torch
 import sys
 import warnings
 warnings.filterwarnings('ignore')
 class YesNoQuestionGenerator:
-  def __init__(self,passage,nquestion):
+  def __init__(self,passage, nquestion):
     self.truefalse = "yes"
     self.text ="truefalse: %s passage: %s </s>" % (passage, self.truefalse)
     self.model = T5ForConditionalGeneration.from_pretrained('ramsrigouthamg/t5_boolean_questions')
-    self.tokenizer = T5Tokenizer.from_pretrained('t5-base')
+    self.tokenizer = T5TokenizerFast.from_pretrained('t5-base')
     self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     self.model.to(self.device)
-    self.encoding = self.tokenizer.encode_plus(self.text, return_tensors="pt")
+    self.encoding = self.tokenizer.encode_plus(self.text, max_length=512, return_tensors="pt", return_overflowing_tokens=True, return_attention_mask=True, pad_to_max_length=True, truncation=True)
     self.input_ids, self.attention_masks = self.encoding["input_ids"].to(self.device), self.encoding["attention_mask"].to(self.device)
     self.nquestion = nquestion
 
   def generate(self):
       topkp_output = self.model.generate(input_ids=self.input_ids,
-                                  attention_mask=self.attention_masks,
-                                  max_length=256,
-                                do_sample=True,
-                                top_k=40,
-                                top_p=0.80,
-                                num_return_sequences=self.nquestion,
-                                  no_repeat_ngram_size=2,
-                                  early_stopping=True
-                                )
-      Questions = [self.tokenizer.decode(out, skip_special_tokens=True,clean_up_tokenization_spaces=True) for out in topkp_output]
-      return [Question.strip().capitalize() for Question in Questions]
+                                         attention_mask=self.attention_masks,
+                                         max_length=512,
+                                         do_sample=True,
+                                         top_k=40,
+                                         top_p=0.80,
+                                         num_return_sequences=1,
+                                         no_repeat_ngram_size=2,
+                                         early_stopping=True
+                                        )
+      #Questions = [self.tokenizer.decode(out, skip_special_tokens=True,clean_up_tokenization_spaces=True) for out in topkp_output]
+      topk_decoded_questions = self.tokenizer.batch_decode(topkp_output, skip_special_tokens=True, clean_up_tokenization_spaces=True)
+      return [question.strip().capitalize() for nq, question in enumerate(topk_decoded_questions) if nq < self.nquestion]
 
-# if __name__ == '__main__':
-#     input_file = sys.argv[1]
-#     N = int(sys.argv[2])
-#
-#     with open(input_file, encoding="UTF-8") as a:
-#         article = a.read()
-#
-#     qg = YesNoQuestionGenerator(article, N)
-#     output = qg.generate()
-#     for out in output:
-#         print(out)
+if __name__ == '__main__':
+     input_file = sys.argv[1]
+     N = int(sys.argv[2])
+
+     with open(input_file, encoding="UTF-8") as a:
+         article = a.read()
+
+     qg = YesNoQuestionGenerator(article, N)
+     output = qg.generate()
+     for out in output:
+         print(out)
 
 
