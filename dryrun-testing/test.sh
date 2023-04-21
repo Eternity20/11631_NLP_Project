437962b0889b0db45cab276d5dcdf39355b97713
EC2 Default User <ec2-user@ip-172-31-94-242.ec2.internal>
2023-04-21 22:45:13 +0000

    finish the docker download

--- dryrun-testing/test.sh
+++ dryrun-testing/test.sh
@@ -4,6 +4,6 @@ NAME=$(sudo docker run --runtime=nvidia -d -v ${SHARE}:/host/Users -it ${IMAGE} 
 echo '****************'
 docker exec -i $NAME ./ask /host/Users/a7.txt 5 2>/dev/null
 echo '****************'
-# docker exec -i $NAME ./answer /host/Users/a7.txt /host/Users/a7_questions.txt
-# echo '****************'
+docker exec -i $NAME ./answer /host/Users/a7.txt /host/Users/a7_questions.txt
+echo '****************'
 docker stop $NAME >/dev/null
