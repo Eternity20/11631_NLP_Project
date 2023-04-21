b71c606f0730bdfa825da00c4d6f297ad7bea042
Gustavo <ggoncav@cs.cmu.edu>
2023-04-21 16:12:10 -0400

    Revert test.sh

--- test.sh
+++ test.sh
@@ -2,9 +2,9 @@ SHARE=$(pwd)
 IMAGE=$1
 NAME=$(sudo docker run --runtime=nvidia -d -v ${SHARE}:/host/Users -it ${IMAGE} /bin/bash)
 echo '*******QG*********'
-sudo docker exec -i $NAME ./ask /host/Users/data/wikipedia_text/pokemon_characters/pikachu_pokemon.txt 3 2>/dev/null
+sudo docker exec -i $NAME ./ask /host/Users/data/wikipedia_text/pokemon_characters/pikachu_pokemon.txt 3
 # sudo docker exec -i $NAME ./ask /host/Users/data/set1/a7.txt 3 2>/dev/null
 echo '*******QA*********'
-sudo docker exec -i $NAME ./answer /host/Users/data/wikipedia_text/pokemon_characters/pikachu_pokemon.txt /host/Users/test_questions.txt 2>/dev/null
+sudo docker exec -i $NAME ./answer /host/Users/data/wikipedia_text/pokemon_characters/pikachu_pokemon.txt /host/Users/test_questions.txt
 echo '*****Finish********'
 sudo docker stop $NAME >/dev/null
