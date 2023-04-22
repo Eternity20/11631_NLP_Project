c0bbf1b34f044f84910edc47a1cea3e68b4bffa1
EC2 Default User <ec2-user@ip-172-31-83-16.ec2.internal>
2023-04-22 03:24:54 +0000

    final version

--- dryrun-testing/test.sh
+++ dryrun-testing/test.sh
@@ -2,7 +2,7 @@ SHARE=$(pwd)
 IMAGE=$1
 NAME=$(sudo docker run --runtime=nvidia -d -v ${SHARE}:/host/Users -it ${IMAGE} /bin/bash)
 echo '****************'
-docker exec -i $NAME ./ask /host/Users/a7.txt 5 2>/dev/null
+docker exec -i $NAME ./ask /host/Users/a7.txt 5
 echo '****************'
 docker exec -i $NAME ./answer /host/Users/a7.txt /host/Users/a7_questions.txt
 echo '****************'
