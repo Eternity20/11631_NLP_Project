8462729d2c5db428cd2910a6f39e1ac3d27a3221
Maggie <zhongyuezhang785@gmail.com>
2023-04-25 20:36:44 -0400

    add content to docker

---
+++ docker/test_energy_ask.py
@@ -0,0 +1,7 @@
+import energyusage
+
+# user function to be evaluated
+def recursive_fib(n):
+    if (n <= 2): return 1
+    else: return recursive_fib(n-1) + recursive_fib(n-2)
+energyusage.evaluate(recursive_fib, 40, pdf=False)
