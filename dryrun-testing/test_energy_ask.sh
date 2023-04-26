45d2bb8e0e5d3ec96adcb1b60aa0ef72d606f2b2
Maggie <zhongyuezhang785@gmail.com>
2023-04-25 22:07:25 -0400

    add with energy script

---
+++ dryrun-testing/test_energy_ask.sh
@@ -0,0 +1,6 @@
+SHARE=$(pwd)
+IMAGE=$1
+NAME=$(sudo docker run --runtime=nvidia -d -v ${SHARE}:/host/Users -it ${IMAGE} /bin/bash)
+echo '****************'
+#docker exec -i $NAME ./test_cpu_ask.py /host/Users/a7.txt 5 1
+docker exec -i $NAME ./test_energy_ask.py /host/Users/a7.txt 5 0
